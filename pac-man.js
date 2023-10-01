2a0488067a94addd29dc6bc421138a3a8ad38ba0
Netra patel <136146102+Netra321@users.noreply.github.com>
2023-10-01 15:41:39 +0530

    remove extra space

--- pac-man.js
+++ pac-man.js
@@ -8,7 +8,7 @@
 setInterval(gameloop,1000/75);*/
 
 document.addEventListener('DOMContentLoaded', () => {
-
+//total box 20*20=400
     const width = 20
     const grid = document.querySelector('.grid')
     const layout = [
@@ -32,7 +32,7 @@ document.addEventListener('DOMContentLoaded', () => {
         1,0,1,1,0,1,0,1,1,0,1,0,1,0,1,0,1,1,0,1,   //2
         1,3,0,0,0,1,0,0,0,0,1,0,0,0,1,0,0,0,3,1,   //1
         1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1    //0
-    /*  0,1,2,3,4,5,6,7,8,9,9,8,7,6,5,4,3,2,1,0*/
+    //  0,1,2,3,4,5,6,7,8,9,9,8,7,6,5,4,3,2,1,0
        ]
     const squares = []
   
@@ -65,13 +65,40 @@ document.addEventListener('DOMContentLoaded', () => {
           squares[i].classList.add('power-pellet')
         }
         
-        else if (layout[i] === 4) 
-          {
-            squares[i].classList.add('pacman')
-          }
       }
     }
     createBoard()
   
+    let pacmanCurrentplace = 372
+    squares[pacmanCurrentplace].classList.add('pacman')  
+
+    function movement(e) {
+      squares[pacmanCurrentplace].classList.remove('pacman')
+      switch(e.keyCode) {
+       case 37://left move
+
+          if(pacmanCurrentplace % width !== 0) pacmanCurrentplace -= 1
+          break
+
+       case 38://up move
+          
+          if(pacmanCurrentplace - width >= 0) pacmanCurrentplace -= width
+          break
+
+        case 39://right move
+
+          if(pacmanCurrentplace % width < width - 1) pacmanCurrentplace += 1
+          break
+
+        case 40://down move
+          
+          if (pacmanCurrentplace + width < width * width)pacmanCurrentplace += width
+          break
+      }
+      squares[pacmanCurrentplace].classList.add('pacman')
+     
+    }
+    document.addEventListener('keyup', movement)
   
+
 })
